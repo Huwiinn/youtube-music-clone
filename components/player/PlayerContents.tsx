4108f2c3bd9a2ccd99950bd2616f437aaa9b6390
hwuiinn <gnldls8406@gmail.com>
2025-11-01 18:18:12 +0900

    feat: 노래 재생

--- components/player/PlayerContents.tsx
+++ components/player/PlayerContents.tsx
@@ -1,12 +1,67 @@
 import React from "react";
 import { Slider as PlayerSlider } from "@/components/ui/playerSlider";
+import { useAudio } from "react-use";
+import { IoPlaySkipBackSharp, IoPlaySkipForwardSharp } from "react-icons/io5";
+import { AiOutlinePause } from "react-icons/ai";
+import usePlayerState from "@/hooks/usePlayerState";
+import { ClipLoader } from "react-spinners";
+import { RiPlayFill } from "react-icons/ri";
 
 const PlayerContents = () => {
+  const { activeSong } = usePlayerState();
+  const [audio, state, controls, ref] = useAudio({
+    src: activeSong?.src,
+    autoPlay: true,
+  });
+
+  const isLoading = activeSong?.src && state.buffered?.length === 0;
+
+  const onClickPreBtn = () => {};
+  const onClickStartBtn = () => {
+    controls.play();
+  };
+  const onClickPauseBtn = () => {
+    controls.pause();
+  };
+  const onClickNextBtn = () => {};
+
   return (
     <div className="h-full w-full relative">
       <div className="absolute top-[-16px] w-full">
         <PlayerSlider />
       </div>
+      {audio}
+      <section className="flex justify-between items-center w-full h-full px-2 lg:px-6">
+        <div className="flex items-center h-full gap-1 lg:gap-8">
+          <IoPlaySkipBackSharp
+            size={24}
+            className="cursor-pointer"
+            onClick={onClickPreBtn}
+          />
+          {isLoading ? (
+            <ClipLoader color="#fff" />
+          ) : state.playing ? (
+            <AiOutlinePause
+              size={40}
+              className="cursor-pointer"
+              onClick={onClickPauseBtn}
+            />
+          ) : (
+            <RiPlayFill
+              size={40}
+              className="cursor-pointer"
+              onClick={onClickStartBtn}
+            />
+          )}
+          <IoPlaySkipForwardSharp
+            size={24}
+            className="cursor-pointer"
+            onClick={onClickNextBtn}
+          />
+        </div>
+        <article></article>
+        <div></div>
+      </section>
     </div>
   );
 };
